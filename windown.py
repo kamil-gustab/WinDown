f8364c89f4ce6d7953c3e5e7ba19e325f0fc03be
kamil-gustab <gustab.kamil@gmail.com>
2022-01-14 23:48:33 +0100

    Small changes.

--- windown.py
+++ windown.py
@@ -2,18 +2,14 @@ import os
 from time import sleep
 
 
-def date():
-    return os.system('cmd /c "date"')
-
-
 def shutdown(time=60, force=False, restart=False):
     """Shutting down computer after time (seconds) provided as argument"""
 
-    time_in_mins = "less than 1" if time < 60 else int(time/60)
-    comment = f'Computer will be turned off in {time_in_mins} min. Usage of WinDown app.'
-    command = f'"shutdown /c "{comment}" /s /f /t {time}"'
+    formatted_time = f'{time} sec' if time < 60 else f'{int(time/60)} min'
+    comment = f'Computer will be turned off in {formatted_time}. Usage of WinDown app.'
+    command = f'shutdown /c "{comment}" /t {time} /s /f'
 
-    os.system(f'{command}')
+    os.system(command)
 
     sleep(5)
     os.system('shutdown -a')
