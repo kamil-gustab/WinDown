d6498c37a851f3ecc6df7936a81ea2e39b0e0c6e
kamil-gustab <gustab.kamil@gmail.com>
2022-01-14 18:01:07 +0100

    Basic changes.

--- windown.py
+++ windown.py
@@ -1,15 +1,24 @@
 import os
+from time import sleep
 
 
 def date():
-    date = os.system('cmd /c "date"')
-    return date
+    return os.system('cmd /c "date"')
+
 
 def shutdown(time=60, force=False, restart=False):
-    ''' Shutting down computer after time (seconds) provided as argument '''
-    os.system(f'cmd /c "shutdown /c "Usage of WinDown app." /d u /s /f /t {time}"')
+    """Shutting down computer after time (seconds) provided as argument"""
+
+    time_in_mins = "less than 1" if time < 60 else int(time/60)
+    comment = f'Computer will be turned off in {time_in_mins} min. Usage of WinDown app.'
+    command = f'"shutdown /c "{comment}" /s /f /t {time}"'
+
+    os.system(f'{command}')
+
+    sleep(5)
+    os.system('shutdown -a')
 
 
 if __name__ == '__main__':
 
-    print(shutdown(600))
+    shutdown(40)
