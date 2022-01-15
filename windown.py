1088d17a3452512811c9c3e0a7c3ab7f7113b15d
kamil-gustab <gustab.kamil@gmail.com>
2022-01-15 17:14:47 +0100

    Changing typos.

--- windown.py
+++ windown.py
@@ -2,12 +2,13 @@ import os
 from time import sleep
 
 
-def shutdown(time=60, force=False, restart=False):
+def shutdown(time=60, force='', restart=''):
     """Shutting down computer after time (seconds) provided as argument"""
 
     formatted_time = f'{time} sec' if time < 60 else f'{int(time/60)} min'
     comment = f'Computer will be turned off in {formatted_time}. Usage of WinDown app.'
-    command = f'shutdown /c "{comment}" /t {time} /s /f'
+    command = f'shutdown {force} {restart} /c "{comment}" /t {time} /s'
+    print(f'\n{command}\n')
 
     os.system(command)
 
@@ -17,4 +18,5 @@ def shutdown(time=60, force=False, restart=False):
 
 if __name__ == '__main__':
 
-    shutdown(40)
+    # shutdown(time=40, force='/f', restart='/r')
+    shutdown(time=40, force='/f')
