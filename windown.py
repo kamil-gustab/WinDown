b1f2dcd4b8ca705d676b69a63276dc57ea0653d6
Kamil Gustab <kamil.gustab@twoupdigital.com>
2022-02-06 19:45:12 +0100

    working

--- windown.py
+++ windown.py
@@ -23,7 +23,7 @@
 
 from datetime import datetime
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import sys
@@ -51,6 +51,10 @@ class Window(QMainWindow):
     def UiComponents(self):
         self.hour = int(datetime.now().strftime("%H"))-3
         self.minute = int(datetime.now().strftime("%M"))
+
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(200, 320, 61, 31))
+        self.lineEdit.setObjectName("lineEdit")
 
         # creating a label to show the time
         self.label = QLabel(self)
@@ -149,3 +153,65 @@ if __name__ == '__main__':
     App = QApplication(sys.argv)
     window = Window()
     sys.exit(App.exec())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # from PyQt5 import QtCore, QtGui, QtWidgets
+    #
+    #
+    # class Ui_MainWindow(object):
+    #     def setupUi(self, MainWindow):
+    #         MainWindow.setObjectName("MainWindow")
+    #         MainWindow.resize(800, 600)
+    #         self.centralwidget = QtWidgets.QWidget(MainWindow)
+    #         self.centralwidget.setObjectName("centralwidget")
+    #
+    #         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+    #         self.lineEdit.setGeometry(QtCore.QRect(200, 320, 61, 31))
+    #         self.lineEdit.setObjectName("lineEdit")
+    #
+    #         MainWindow.setCentralWidget(self.centralwidget)
+    #         self.menubar = QtWidgets.QMenuBar(MainWindow)
+    #         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+    #         self.menubar.setObjectName("menubar")
+    #         MainWindow.setMenuBar(self.menubar)
+    #         self.statusbar = QtWidgets.QStatusBar(MainWindow)
+    #         self.statusbar.setObjectName("statusbar")
+    #         MainWindow.setStatusBar(self.statusbar)
+    #
+    #         self.retranslateUi(MainWindow)
+    #         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    #
+    #     def retranslateUi(self, MainWindow):
+    #         _translate = QtCore.QCoreApplication.translate
+    #         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+    #
+    #
+    # if __name__ == "__main__":
+    #     import sys
+    #
+    #     app = QtWidgets.QApplication(sys.argv)
+    #     MainWindow = QtWidgets.QMainWindow()
+    #     ui = Ui_MainWindow()
+    #     ui.setupUi(MainWindow)
+    #     MainWindow.show()
+    #     sys.exit(app.exec_())
+    #
