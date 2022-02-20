e8a16a35cd183c24e1c833804fef3b69067631da
kamil-gustab <gustab.kamil@gmail.com>
2022-02-20 22:48:21 +0100

    Many changes, ongoing...

--- windown.py
+++ windown.py
@@ -52,28 +52,25 @@ class Window(QMainWindow):
         self.hour = int(datetime.now().strftime("%H"))-3
         self.minute = int(datetime.now().strftime("%M"))
 
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(200, 320, 61, 31))
-        self.lineEdit.setObjectName("lineEdit")
+        # self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        # self.lineEdit.setGeometry(QtCore.QRect(200, 320, 61, 31))
+        # self.lineEdit.setObjectName("lineEdit")
 
-        # creating a label to show the time
-        self.label = QLabel(self)
+        # creating a time_box to show the time
+        self.time_box = QPushButton(self)
 
         # setting geometry of label
-        self.label.setGeometry(125, 100, 150, 70)
+        self.time_box.setGeometry(125, 100, 150, 70)
 
         # adding border to the label
-        self.label.setStyleSheet("border : 4px solid black;")
+        self.time_box.setStyleSheet("border : 4px solid black;")
 
         # setting text to the label
         time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-        self.label.setText(time_now)
+        self.time_box.setText(time_now)
 
         # setting font to the label
-        self.label.setFont(QFont('Arial', 25))
-
-        # setting alignment to the text of label
-        self.label.setAlignment(Qt.AlignCenter)
+        self.time_box.setFont(QFont('Arial', 25))
 
         # managing hours
         hour_plus = QPushButton("+", self)
@@ -94,57 +91,81 @@ class Window(QMainWindow):
         min_minus.pressed.connect(self.Min_minus)
 
         # creating reset button
-        re_set = QPushButton("Re-set", self)
+        reset_button = QPushButton("Reset", self)
+        go_button = QPushButton("Request Shutdown", self)
 
         # setting geometry to the button
-        re_set.setGeometry(125, 420, 150, 40)
+        reset_button.setGeometry(125, 420, 150, 40)
+        go_button.setGeometry(125, 370, 150, 40)
 
         # add action to the method
-        re_set.pressed.connect(self.Re_set)
+        reset_button.pressed.connect(self.Re_set)
+
+        # # label
+        # self.testlabel = QLabel(self)
+        # self.testlabel.setGeometry(QtCore.QRect(95, 0, 110, 40))
+        # self.testlabel.setText("XD")
+
+        self.time_box.clicked.connect(lambda: self.show_input())
+
 
 
     def showTime(self):
         time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-        self.label.setText(time_now)
+        self.time_box.setText(time_now)
 
     # Hour operations
     def Hour_plus(self):
-        if self.hour != 23:
+        if self.hour < 23:
             self.hour += 1
             time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-            self.label.setText(time_now)
+            self.time_box.setText(time_now)
 
     def Hour_minus(self):
-        if self.hour != 0:
+        if self.hour > 0:
             self.hour -= 1
             time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-            self.label.setText(time_now)
+            self.time_box.setText(time_now)
 
     # Minutes operations
     def Min_plus(self):
-        if self.minute != 59:
+        if self.minute < 59:
             self.minute += 1
             time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-            self.label.setText(time_now)
+            self.time_box.setText(time_now)
 
     def Min_minus(self):
-        if self.minute != 0:
+        if self.minute > 0:
             self.minute -= 1
             time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-            self.label.setText(time_now)
+            self.time_box.setText(time_now)
 
     def Re_set(self):
-        # making flag to false
-        self.flag = False
-
         # reseeting the hour
         self.hour = 0
         self.minute = 0
 
-        # setting text to label
-        # self.label.setText(str(self.hour))
+        # setting text to time_box
         time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-        self.label.setText(time_now)
+        self.time_box.setText(time_now)
+
+    def show_input(self):
+        # this has to happen on click on label with hour (or another one)
+        text, pressed = QInputDialog.getText(self, 'Time', 'Use "HH:MM" format.',
+                                             QLineEdit.Normal, '')
+
+        try:
+            if pressed:
+                if text[2] == ':' and len(text)==5:
+                    hour_ = int(text[:2])
+                    if 0 <= hour_ <= 23: self.hour = hour_
+                    min_ = int(text[3:5])
+                    if 0 <= min_ <= 59: self.minute = min_
+                    time_now = f'{(self.hour):02d}:{(self.minute):02d}'
+                    self.time_box.setText(time_now)
+        except ValueError:
+            QMessageBox.about(self, "Title", "Message")
+
 
 if __name__ == '__main__':
 
@@ -153,65 +174,3 @@ if __name__ == '__main__':
     App = QApplication(sys.argv)
     window = Window()
     sys.exit(App.exec())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # from PyQt5 import QtCore, QtGui, QtWidgets
-    #
-    #
-    # class Ui_MainWindow(object):
-    #     def setupUi(self, MainWindow):
-    #         MainWindow.setObjectName("MainWindow")
-    #         MainWindow.resize(800, 600)
-    #         self.centralwidget = QtWidgets.QWidget(MainWindow)
-    #         self.centralwidget.setObjectName("centralwidget")
-    #
-    #         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-    #         self.lineEdit.setGeometry(QtCore.QRect(200, 320, 61, 31))
-    #         self.lineEdit.setObjectName("lineEdit")
-    #
-    #         MainWindow.setCentralWidget(self.centralwidget)
-    #         self.menubar = QtWidgets.QMenuBar(MainWindow)
-    #         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-    #         self.menubar.setObjectName("menubar")
-    #         MainWindow.setMenuBar(self.menubar)
-    #         self.statusbar = QtWidgets.QStatusBar(MainWindow)
-    #         self.statusbar.setObjectName("statusbar")
-    #         MainWindow.setStatusBar(self.statusbar)
-    #
-    #         self.retranslateUi(MainWindow)
-    #         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-    #
-    #     def retranslateUi(self, MainWindow):
-    #         _translate = QtCore.QCoreApplication.translate
-    #         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-    #
-    #
-    # if __name__ == "__main__":
-    #     import sys
-    #
-    #     app = QtWidgets.QApplication(sys.argv)
-    #     MainWindow = QtWidgets.QMainWindow()
-    #     ui = Ui_MainWindow()
-    #     ui.setupUi(MainWindow)
-    #     MainWindow.show()
-    #     sys.exit(app.exec_())
-    #
