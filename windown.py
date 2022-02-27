5ee424c4e0ed8b20771cd3e3a4759b4f63e3fe1e
kamil-gustab <gustab.kamil@gmail.com>
2022-02-27 20:45:07 +0100

    Working fine. Version 1.0.

--- windown.py
+++ windown.py
@@ -1,176 +1,132 @@
-# import os
-# from time import sleep
-#
-#
-# def shutdown(time=60, force='', reHour_operation=''):
-#     """Shutting down computer after time (seconds) provided as argument"""
-#
-#     formatted_time = f'{time} sec' if time < 60 else f'{int(time/60)} min'
-#     comment = f'Computer will be turned off in {formatted_time}. Usage of WinDown app.'
-#     command = f'shutdown {force} {reHour_operation} /c "{comment}" /t {time} /s'
-#     print(f'\n{command}\n')
-#
-#     os.system(command)
-#
-#     sleep(5)
-#     os.system('shutdown -a')
-#
-#
-# if __name__ == '__main__':
-#
-#     # shutdown(time=40, force='/f', reHour_operation='/r')
-#     shutdown(time=40, force='/f')
-
 from datetime import datetime
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+import operations
 import sys
-
 
 
 class Window(QMainWindow):
 
     def __init__(self):
         super().__init__()
-
-        # setting title
         self.setWindowTitle("WinDown")
-
-        # setting geometry
         self.setGeometry(100, 100, 400, 500)
-
-        # calling method
         self.UiComponents()
-
-        # showing all the widgets
         self.show()
+        self.hour = int(datetime.now().strftime("%H"))
 
     # method for widgets
-    def UiComponents(self):
+    def uicomponents(self):
         self.hour = int(datetime.now().strftime("%H"))
         self.minute = int(datetime.now().strftime("%M"))
-
-        # self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        # self.lineEdit.setGeometry(QtCore.QRect(200, 320, 61, 31))
-        # self.lineEdit.setObjectName("lineEdit")
-
-        # creating a time_box to show the time
         self.time_box = QPushButton(self)
-
-        # setting geometry of label
-        self.time_box.setGeometry(125, 100, 150, 70)
-
-        # adding border to the label
+        self.time_box.setGeometry(125, 120, 150, 70)
         self.time_box.setStyleSheet("border : 4px solid black;")
-
-        # setting text to the label
-        time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-        self.time_box.setText(time_now)
-
-        # setting font to the label
+        shown_time = f'{self.hour:02d}:{self.minute:02d}'
+        self.time_box.setText(shown_time)
         self.time_box.setFont(QFont('Arial', 25))
 
         # managing hours
         hour_plus = QPushButton("+", self)
-        hour_plus.setGeometry(160, 60, 30, 30)
+        hour_plus.setGeometry(160, 80, 30, 30)
         hour_plus.pressed.connect(self.hour_plus)
 
         hour_minus = QPushButton("-", self)
-        hour_minus.setGeometry(160, 180, 30, 30)
+        hour_minus.setGeometry(160, 200, 30, 30)
         hour_minus.pressed.connect(self.hour_minus)
 
         # managing minutes
         min_plus = QPushButton("+", self)
-        min_plus.setGeometry(210, 60, 30, 30)
+        min_plus.setGeometry(210, 80, 30, 30)
         min_plus.pressed.connect(self.min_plus)
 
         min_minus = QPushButton("-", self)
-        min_minus.setGeometry(210, 180, 30, 30)
+        min_minus.setGeometry(210, 200, 30, 30)
         min_minus.pressed.connect(self.min_minus)
 
-        # creating reset button
-        reset_button = QPushButton("Reset", self)
         go_button = QPushButton("Request Shutdown", self)
+        cancel_button = QPushButton("Cancel Shutdown", self)
+        reset_button = QPushButton("Reset clock", self)
 
-        # setting geometry to the button
+        go_button.setGeometry(125, 320, 150, 40)
+        cancel_button.setGeometry(125, 370, 150, 40)
         reset_button.setGeometry(125, 420, 150, 40)
-        go_button.setGeometry(125, 370, 150, 40)
 
-        # add action to the method
+        go_button.pressed.connect(self.request_shutdown)
+        cancel_button.pressed.connect(self.cancel_shutdown)
         reset_button.pressed.connect(self.reset_time)
-
-        # # label
-        # self.testlabel = QLabel(self)
-        # self.testlabel.setGeometry(QtCore.QRect(95, 0, 110, 40))
-        # self.testlabel.setText("XD")
-
         self.time_box.clicked.connect(lambda: self.show_input())
 
-
-
-    def showTime(self):
-        time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-        self.time_box.setText(time_now)
-
-    # Hour operations
+    # Hours operations
     def hour_plus(self):
         if self.hour < 23:
             self.hour += 1
-            time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-            self.time_box.setText(time_now)
+            shown_time = f'{self.hour:02d}:{self.minute:02d}'
+            self.time_box.setText(shown_time)
 
     def hour_minus(self):
         if self.hour > 0:
             self.hour -= 1
-            time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-            self.time_box.setText(time_now)
+            shown_time = f'{self.hour:02d}:{self.minute:02d}'
+            self.time_box.setText(shown_time)
 
     # Minutes operations
     def min_plus(self):
         if self.minute < 59:
             self.minute += 1
-            time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-            self.time_box.setText(time_now)
+            shown_time = f'{self.hour:02d}:{self.minute:02d}'
+            self.time_box.setText(shown_time)
 
     def min_minus(self):
         if self.minute > 0:
             self.minute -= 1
-            time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-            self.time_box.setText(time_now)
+            shown_time = f'{self.hour:02d}:{self.minute:02d}'
+            self.time_box.setText(shown_time)
 
     def reset_time(self):
-        # reseeting the hour
         self.hour = int(datetime.now().strftime("%H"))
         self.minute = int(datetime.now().strftime("%M"))
 
-        # setting text to time_box
-        time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-        self.time_box.setText(time_now)
+        shown_time = f'{self.hour:02d}:{self.minute:02d}'
+        self.time_box.setText(shown_time)
 
     def show_input(self):
-        # this has to happen on click on label with hour (or another one)
         text, pressed = QInputDialog.getText(self, 'Time', 'Use "HH:MM" format.',
                                              QLineEdit.Normal, '')
 
         try:
             if pressed:
-                if text[2] == ':' and len(text)==5:
+                if text[2] == ':' and len(text) == 5:
                     hour_ = int(text[:2])
-                    if 0 <= hour_ <= 23: self.hour = hour_
+                    if 0 <= hour_ <= 23:
+                        self.hour = hour_
                     min_ = int(text[3:5])
-                    if 0 <= min_ <= 59: self.minute = min_
-                    time_now = f'{(self.hour):02d}:{(self.minute):02d}'
-                    self.time_box.setText(time_now)
+                    if 0 <= min_ <= 59:
+                        self.minute = min_
+                    shown_time = f'{self.hour:02d}:{self.minute:02d}'
+                    self.time_box.setText(shown_time)
         except ValueError:
-            QMessageBox.about(self, "Title", "Message")
+            QMessageBox.about(self, 'Error', 'Wrong time format.\nUse format: "HH:MM"\neg. 21:37')
+
+    def request_shutdown(self):
+        # translating string to datetime object
+        time_now = datetime.now()
+        shown_time = f'{str(time_now)[:11]}{self.hour:02d}:{self.minute:02d}'
+        shown_time_obj = datetime.strptime(shown_time, '%Y-%m-%d %H:%M')
+
+        # checking if requested shutdown isn't before actual time
+        if shown_time_obj < datetime.now():
+            return
+        diff_in_sec = (shown_time_obj - time_now).seconds
+
+        operations.shutdown(time=diff_in_sec)
+
+    @staticmethod
+    def cancel_shutdown():
+        operations.cancel_shutdown()
 
 
 if __name__ == '__main__':
-
-    # print("Current Time =", datetime.now().strftime("%H:%M"))
-
     App = QApplication(sys.argv)
     window = Window()
     sys.exit(App.exec())
